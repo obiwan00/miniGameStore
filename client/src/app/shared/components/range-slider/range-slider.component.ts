faee8336b4b85e7723dd58ed85b0769d92e0c99a
Ivan Maidaniuk <ivan.maidaniuk@gmail.com>
2021-09-14 22:54:35 +0300

    Update RangeSliderComponent

--- client/src/app/shared/components/range-slider/range-slider.component.ts
+++ client/src/app/shared/components/range-slider/range-slider.component.ts
@@ -1,33 +1,26 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-range-slider',
   templateUrl: './range-slider.component.html',
   styleUrls: ['./range-slider.component.scss']
 })
-export class RangeSliderComponent implements OnInit, OnChanges {
+export class RangeSliderComponent implements OnChanges {
   @Input() disabled: boolean = false
   @Input() minValue: number = 0
   @Input() maxValue: number = 0
   @Input() currentInputValue: number = -1
   @Output() formEmitter = new EventEmitter<number>()
   public formGroup = new FormGroup({
-    range: new FormControl('0')
+    range: new FormControl(-1)
   })
 
   constructor() { }
 
   ngOnChanges() {
-    this.currentInputValue = Math.ceil(this.currentInputValue);
-    this.maxValue = Math.ceil(this.maxValue);
-    this.currentValue = this.currentInputValue < this.minValue ? this.maxValue :  this.currentInputValue
-
+    this.currentValue = this.currentInputValue < this.minValue ? this.maxValue : this.currentInputValue
     this.disabled ? this.formGroup.disable() : this.formGroup.enable()
-  }
-
-  ngOnInit(): void {
-
   }
 
   get rangeControl() {
